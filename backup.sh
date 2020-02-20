19fe671b01a2748304a502a5ad81e8e5d5cc5e72
doob187 <60312740+doob187@users.noreply.github.com>
2020-02-20 17:02:21 +0100

    exclude traktarr folder (#1)
    * exclude traktarr folder 
    
    exclude traktarr folder 
    add remove part for old backups
    
    * Update backup.sh
    
    * Update backup.sh
    
    * Update backup.sh
    
    * Update backup.sh
    
    * Update backup.sh
    
    * Update backup.sh

--- backup.sh
+++ backup.sh
@@ -18,18 +18,17 @@
 #   want to change anything unless you  #
 #   know what you're doing.             #
 #########################################
-
 PIDFILE=/var/run/backup.pid
-
 # Skip compression on already compressed files
-RSYNC_SKIP_COMPRESS="3fr/3g2/3gp/3gpp/7z/aac/ace/amr/apk/appx/appxbundle/arc/arj/arw/asf/avi/bz2/cab/cr2/crypt[5678]/dat/dcr/deb/dmg/drc/ear/erf/flac/flv/gif/gpg/gz/iiq/iso/jar/jp2/jpeg/jpg/k25/kdc/lz/lzma/lzo/m4[apv]/mef/mkv/mos/mov/mp[34]/mpeg/mp[gv]/msi/nef/oga/ogg/ogv/opus/orf/pef/png/qt/rar/rpm/rw2/rzip/s7z/sfx/sr2/srf/svgz/t[gb]z/tlz/txz/vob/wim/wma/wmv/xz/zip"
+#RSYNC_SKIP_COMPRESS="3fr/3g2/3gp/3gpp/7z/aac/ace/amr/apk/appx/appxbundle/arc/arj/arw/asf/avi/bz2/cab/cr2/crypt[5678]/dat/dcr/deb/dmg/drc/ear/erf/flac/flv/gif/gpg/gz/iiq/iso/jar/jp2/jpeg/jpg/k25/kdc/lz/lzma/lzo/m4[apv]/mef/mkv/mos/mov/mp[34]/mpeg/mp[gv]/msi/nef/oga/ogg/ogv/opus/orf/pef/png/qt/rar/rpm/rw2/rzip/s7z/sfx/sr2/srf/svgz/t[gb]z/tlz/txz/vob/wim/wma/wmv/xz/zip"
+# --skip-compress=$RSYNC_SKIP_COMPRESS \
+#--numeric-ids
 
 # Options to pass to rsync
 OPTIONS="--force --ignore-errors --delete \
  --exclude-from=/root/backup_excludes \
- --skip-compress=$RSYNC_SKIP_COMPRESS \
  --backup --backup-dir=$ARCHIVEROOT \
- -aHAXxvP --numeric-ids"
+ -aqHAXxv"
 
 OPTIONSTAR="--warning=no-file-changed \
   --ignore-failed-read \
@@ -41,7 +40,6 @@ OPTIONSTAR="--warning=no-file-changed \
 OPTIONSRCLONE="--config /rclone/rclone.conf \
  -v --size-only --stats-one-line --stats 1s --progress --tpslimit=10 \
  --checkers=8 --transfers=4 --no-traverse --fast-list"
-
 INCREMENT=$(date +%Y-%m-%d)
 
 # Make sure our backup tree exists
@@ -55,6 +53,7 @@ do_rsync()
  # shellcheck disable=SC2164
   rsync ${OPTIONS} -e "ssh -Tx -c aes128-gcm@openssh.com -o Compression=no -i ${SSH_IDENTITY_FILE} -p${SSH_PORT}" "${BACKUPDIR}/" "$ARCHIVEROOT"
 }
+
 tar_gz()
 {
  # shellcheck disable=SC2086
@@ -63,6 +62,7 @@ tar_gz()
 cd ${ARCHIVEROOT}
 for dir_tar in `find . -maxdepth 1 -type d  | grep -v "^\.$" `; do tar ${OPTIONSTAR} -C ${dir_tar} -cvf ${dir_tar}.tar ./; done
 }
+
 upload_tar()
 {
 # shellcheck disable=SC2164
@@ -73,7 +73,6 @@ if grep -q gcrypt /rclone/rclone.conf; then
  else
   REMOTE="gdrive"
 fi
-
 sid="/rclone/server.id"
 if [ -f $sid ]; then
   echo "$(date) :  ServerID Set to $(cat /rclone/server.id)"
@@ -81,32 +80,51 @@ else
   echo backup >/rclone/server.id
   echo "$(date) :  NO ServerID Found"
 fi
-
-tree -a -L 1 ${ARCHIVEROOT} | awk '{print $2}' | tail -n +2 | head -n -2 | grep ".tar" >/tmp/tar_folders
+tree -a -L 1 ${ARCHIVEROOT} | awk '{print $2}' | tail -n +2 | head -n -2 | grep "*.tar" >/tmp/tar_folders
 p="/tmp/tar_folders"
 
 while read p; do
-
   echo $p >/tmp/tar
   tar=$(cat /tmp/tar)
-  rclone copyto ${ARCHIVEROOT}/${tar} ${REMOTE}:/backup/$(cat /rclone/server.id)/${tar} ${OPTIONSRCLONE}
-  rclone copyto ${ARCHIVEROOT}/${tar} ${REMOTE}:/backup-daily/$(cat /rclone/server.id)/${INCREMENT}/${tar} ${OPTIONSRCLONE}
-
+  rclone moveto ${ARCHIVEROOT}/${tar} ${REMOTE}:/backup/$(cat /rclone/server.id)/${tar} ${OPTIONSRCLONE}
+  rclone moveto ${ARCHIVEROOT}/${tar} ${REMOTE}:/backup-daily/$(cat /rclone/server.id)/${INCREMENT}/${tar} ${OPTIONSRCLONE}
 done </tmp/tar_folders
-
 }
+
 upload_tar_part2()
 {
 rrc="/rclone/rclone.conf"
 if [ -f $rrc ]; then
   upload_tar
   echo "$(date) :  Upload Backup done"
+  remove_old_backups
+  echo "$(date) :  Purge Old Backups done"
+  #remove_old_folder
+  #echo "$(date) :  Old Backup Folder removed  
 else
   echo "$(date) :  NO rclone.conf Found"
   echo "$(date) :  Backups not Uploaded"
+  echo "$(date) :  Backups are overwritted"
 fi
 }
 
+remove_old_backups()
+{
+OPT="--config /rclone/rclone.conf"
+rclone lsf ${REMOTE}:/backup-daily/$(cat /rclone/server.id)/ ${OPT} | head -n -14 >/tmp/backup_old
+p="/tmp/backup_old"
+
+while read p; do
+  echo $p >/tmp/old_backups
+  old_backup=$(cat /tmp/old_backups)
+  rclone delete ${REMOTE}:/backup-daily/$(cat /rclone/server.id)/${old_backup} ${OPT}
+done </tmp/backup_old
+}
+remove_old_folder()
+{
+#NEED ADDON PART
+  find ${ARCHIVEROOT} -mindepth 1 -type d 2>/dev/null -exec rm -rf {} \;
+}
 # Some error handling and/or run our backup and tar_create/tar_upload
 if [ -f $PIDFILE ]; then
   echo "$(date): Backup already running, remove PID file to rerun"
